68cd5fb919419101e7f503f6ccad1b5934ee0478
Andriy Chernetskyy <andriy.chernetskyy99@outlook.com>
2018-10-16 11:22:17 +0300

    Added file output
    Output data to file

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -44,11 +44,16 @@ namespace Task_1
             }
 
             var copiedPeople = new List<Person>(people.Select(x => (Person)x.Clone()));
-            foreach(var i in copiedPeople)
+          
+            using (StreamWriter writer = File.AppendText("OutputData.txt"))
             {
-                Console.WriteLine(i);
+                foreach (var person in copiedPeople)
+                {
+                    writer.WriteLine(person);
+                }
             }
-            
+
+
             Console.ReadLine();
             
 
